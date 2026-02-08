c468dfff1e7875746e770c540b876e243e68758c
Marc Kaltenrieder <tracker@ligulehm.ch>
2026-02-08 22:02:29 +0100

    Fix broken script tag and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
-      manifest: false
-    })
-  ],
-  server: {
-    host: 'app.localhost.test',
-    port: 8080
-  }
+  base: '/DerMechaniker/', // 💡 GitHub Pages base path
+  plugins: [react()]
 });
